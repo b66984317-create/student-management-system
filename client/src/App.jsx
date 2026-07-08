43280a64501b3dc77713a6bea245a4aaf3181f25
b66984317-create <b66984317@gmail.com>
2026-07-08 22:28:19 +0530

    feat: add React Router and wire Navbar links

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css';
+import { BrowserRouter } from 'react-router-dom';
 import MainLayout from './layouts/MainLayout';
 
 function App() {
   return (
-    <MainLayout>
-      <h1>Welcome to the Student Management System</h1>
-    </MainLayout>
+    <BrowserRouter>
+      <MainLayout>
+        <h1>Welcome to the Student Management System</h1>
+      </MainLayout>
+    </BrowserRouter>
   );
 }
 
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,11 +1,13 @@
+import { Link } from 'react-router-dom';
+
 function Navbar() {
   return (
     <nav className="navbar">
       <ul className="navbar-list">
-        <li className="navbar-item">Dashboard</li>
-        <li className="navbar-item">Students</li>
-        <li className="navbar-item">Teachers</li>
-        <li className="navbar-item">Settings</li>
+        <li className="navbar-item"><Link to="/">Dashboard</Link></li>
+        <li className="navbar-item"><Link to="/students">Students</Link></li>
+        <li className="navbar-item"><Link to="/teachers">Teachers</Link></li>
+        <li className="navbar-item"><Link to="/settings">Settings</Link></li>
       </ul>
     </nav>
   );
